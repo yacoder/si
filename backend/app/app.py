434509b078099108e057e4171c689d5905a32b89
vovochka <vgramagin@gmail.com>
2025-03-28 11:16:56 -0400

    removed duplicate code

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, request, send_from_directory
 
 # from flask_cors import CORS
+from flask_sock import Sock
 
 from backend.api.sample_db_handler import get_saved_data_api
 from backend.api.generic_data_provider import get_data_api
@@ -20,6 +21,7 @@ game_data_provider = GameDataProvider(get_data_api())
 
 
 app = Flask(__name__, static_folder='../../frontend/build', static_url_path='/')
+sock = Sock(app)
 
 # CORS(app)
 
@@ -129,7 +131,12 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-
+@sock.route('/ws')
+def websocket_connection(ws):
+    while True:
+        data = ws.receive()  # Receive a message from the client
+        print(f"Received: {data}")
+        ws.send(f"Echo: {data}")  # Send a response back to the client
 
 
 
